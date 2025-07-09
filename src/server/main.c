b630bba678fd2149cead4dde5b312e494b97833d
CarlAugust <carlaugust.sponberg@gmail.com>
2025-07-09 21:55:57 +0200

    Basic tcp server setup

--- src/server/main.c
+++ src/server/main.c
@@ -1,7 +1,152 @@
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+#include <signal.h>
+
+#define SERVER_PORT 8080
+#define MAX_CLIENTS 10
+#define MAX_EVENTS MAX_CLIENTS * 2
+
+int server_fd;
+
+volatile sig_atomic_t shutdown_requested = 0;
+static void handle_sigint(int sig)
+{
+    shutdown_requested = 1;
+    shutdown(server_fd, SHUT_RDWR);
+    close(server_fd);
+}
 
 int main(void)
-{
+{  
+    struct sigaction sa;
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Socket");
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    int opt = 1;
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        perror("server setsockopt");
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in addr_in;
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_addr.s_addr = INADDR_ANY;
+    addr_in.sin_port = htons(SERVER_PORT);
+
+    struct sockaddr* addr = (struct sockaddr *)&addr_in;
+    socklen_t addr_len = sizeof(addr_in);
+
+    if(bind(server_fd, addr, addr_len) == -1)
+    {
+        perror("Bind");
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    if(listen(server_fd, MAX_CLIENTS))
+    {
+        perror("Listen");
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    int epfd = epoll_create(1);
+    struct epoll_event ev = {.data = {.fd = server_fd},
+                             .events = EPOLLIN | EPOLLOUT | EPOLLET };
     
+    if(epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev) == -1)
+    {
+        perror("epoll_ctl");
+        close(server_fd);
+        close(epfd);
+        return EXIT_FAILURE;
+    }
+    
+    struct epoll_event events[MAX_EVENTS];
+    int nfds;
+    while (!shutdown_requested) 
+    {
+        if ((nfds = epoll_wait(epfd, events, MAX_EVENTS, 1000)) == -1)
+        {
+            perror("epoll_wait");
+            close(server_fd);
+            close(epfd);
+            return EXIT_FAILURE;
+        }
+        for (int i = 0; i < nfds; i++) 
+        {
+            if (events[i].data.fd == server_fd)
+            {
+                int client_fd = accept(server_fd, addr, &addr_len);
+                if (client_fd == -1)
+                {
+                    perror("accept");
+                    continue;
+                }
+
+                if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+                {
+                    perror("client setsockopt");
+                    close(client_fd);
+                    continue;
+                }
+
+                struct epoll_event client_ev;
+                client_ev.data.fd = client_fd;
+                client_ev.events = EPOLLIN | EPOLLRDHUP | EPOLLHUP | EPOLLET;
+                if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &client_ev) == -1)
+                {
+                    perror("epoll_ctl add client");
+                    close(client_fd);
+                    continue;
+                }
+            }
+            else if (events[i].events & EPOLLIN)
+            {
+                int fd = events[i].data.fd;
+                char buffer[512] = {0};
+                int n = read(fd, buffer, sizeof(buffer));
+                if (n > 0)
+                {
+                    printf("Received from client %d: %.*s\n", fd, n, buffer);
+                    send(fd, buffer, n, 0);
+                }
+                else if (n == 0)
+                {
+                    printf("Client %d disconnected\n", fd);
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+                    close(fd);
+                }
+                else
+                {
+                    perror("read");
+                }
+            }
+            else
+            {
+                printf("Unexpected event from client %d\n", events[i].data.fd);
+            }
+        }
+    }
+
+    close(epfd);
+
     return EXIT_SUCCESS;
 }
